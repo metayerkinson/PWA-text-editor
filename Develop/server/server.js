77ac4899935c18d2204409a6f2cada675ba0c180
Your Name <metayerkinson@gmail.com>
2023-07-31 16:36:11 -0400

    first commit

---
+++ Develop/server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("../client/dist"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// link routes
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
